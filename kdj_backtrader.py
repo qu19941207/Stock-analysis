efba1e660e56423a01ed3a09e8d872749961ca5c
Cyclonestab <34879467+qu19941207@users.noreply.github.com>
2019-10-05 19:29:29 +0800

    Done

---
+++ kdj_backtrader.py
@@ -0,0 +1,159 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+import pandas as pd
+from numpy import *
+
+# Import the backtrader platform
+import backtrader as bt
+
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
+             
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25).subplot = True
+        self.stoc = bt.indicators.Stochastic(self.datas[0],period=9)
+        self.macd = bt.indicators.MACDHisto(self.datas[0])
+        
+        #bt.indicators.CrossOver(self.stoc.lines.percK[i],self.stoc.lines.percD[i])
+        #rsi = bt.indicators.RSI(self.datas[0])
+        #bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        #bt.indicators.ATR(self.datas[0]).plot = False
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        signal = self.macd.lines.signal
+        histo = self.macd.lines.histo
+        macd = self.macd.lines.macd
+        k = self.stoc.lines.percK
+        d = self.stoc.lines.percD
+        crit = any([bt.indicators.CrossOver(self.stoc.lines.percK[i],self.stoc.lines.percD[i] == 1.0) for i in arange(1,11)])
+
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if crit and histo[0] > 0 :
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.order_target_percent(target=1.0)
+
+        else:
+
+            if histo[0] < 0 :
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.order_target_percent(target=0.0)
+
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    cerebro.addstrategy(TestStrategy)
+    
+    # Create a Data Feed
+    data = bt.feeds.GenericCSVData(
+        dataname='orcl.csv',
+        fromdate=datetime.datetime(2017, 1, 1),
+        todate=datetime.datetime(2017, 12, 31),
+        datetime=1,
+        dtformat=('%Y-%m-%d'),
+        open=5,
+        close=2,
+        high=3,
+        low=4,
+        reverse=False)
+    
+    df = pd.read_csv('intc.csv', parse_dates=True, index_col=1)
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(1000.0)
+
+    # Add a FixedSize sizer according to the stake
+    cerebro.addsizer(bt.sizers.FixedSize, stake=15)
+
+    # Set the commission
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    cerebro.plot()
---
+++ return_correlation_table.py
@@ -0,0 +1,54 @@
+import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
+from pandas_datareader import data as web
+import pickle
+import requests
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['xtick.labelsize'] = 2
+mpl.rcParams['ytick.labelsize'] = 2
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+soup = bs.BeautifulSoup(resp.text, 'lxml')
+table = soup.find('table', {'class': 'wikitable sortable'})
+tickers = []
+for row in table.findAll('tr')[1:]:
+    ticker = row.findAll('td')[0].text
+    tickers.append(ticker)
+tickers = sorted(tickers)
+
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df.set_index('date', inplace=True) 
+    df_corr = df.pct_change().corr()
+    print(df_corr.head())
+    df_corr.to_csv('sp500corr.csv')
+
+    data1 = df_corr.values
+    fig1 = plt.figure(figsize=(13,13))
+    ax1 = fig1.add_subplot(111)
+
+    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+
+    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+    ax1.invert_yaxis()
+    ax1.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax1.set_xticklabels(column_labels)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1,1)
+    plt.tight_layout()
+    #plt.savefig("correlations.png", dpi = (300))
+    plt.show()
+
+visualize_data()
